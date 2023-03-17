49503982ab5d9695d8d136c4388b98a4f84b3d24
Waree Ellis <ellis.waree@gmail.com>
2023-03-17 15:19:16 -0500

    Added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
-
+import Nav from './Nav.js';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+import Section from './Section';
 function App() {
   return (
-    <div className="App">
-      homepage
-    </div>
+    <>
+      <Nav>
+
+      </Nav>
+      <Header>
+
+      </Header>
+      <Main>
+        <Section></Section>
+        <Section></Section>
+        <Section></Section>
+
+      </Main>
+      <Footer>
+
+      </Foo>
+    </>
   );
 }
 
---
+++ src/Section.js
@@ -0,0 +1,12 @@
+function Section() {
+    return (
+
+        <section>
+
+        </section>
+
+
+    );
+}
+
+export default Section;
